7b6d1b52d02c0a5aa5b5fe53b02cd961dd2c37f6
hyperion8atelier <hyperion8atelier@gmail.com>
2026-09-23 21:33:18 -0400

    Beta 0.2

--- 1-1/1-1-2/Picture.py
+++ 1-1/1-1-2/Picture.py
@@ -8,7 +8,7 @@ class Position:
         self.y = y
         self.z = z
     def add(self, x=None, y=None, z=None, pos=None):
-        temp = self
+        temp = Position(self.x, self.y, self.z)
         if pos is None:
             temp.x += x
             temp.y += y
@@ -34,25 +34,29 @@ class Rotation():
         self.z = z
     @staticmethod
     def rotate(position, origin, rotation):
-        temp, xy, xz, yz = dist(position, origin)
+        temp = Position(position.x-origin.x, position.y-origin.y, position.z-origin.z)
 
-        temp = Position(position.x, position.y, position.z)
-        calc_rot = Rotation(0,0,0)
+        x = temp.x
+        z = temp.z
 
-        calc_rot.z = np.deg2rad(rotation.z) + np.atan2(temp.y - origin.y, temp.x - origin.x)
+        temp.x = x * np.cos(np.deg2rad(rotation.y)) - z * np.sin(np.deg2rad(rotation.y))
+        temp.z = x * np.sin(np.deg2rad(rotation.y)) + z * np.cos(np.deg2rad(rotation.y))
 
-        temp.x = origin.x + np.cos(calc_rot.z) * xy
-        temp.y = origin.y + np.sin(calc_rot.z) * xy
+        x = temp.x
+        y = temp.y
 
-        calc_rot.y = np.deg2rad(rotation.y) + np.atan2(temp.z - origin.z, temp.x - origin.x)
+        temp.x = x * np.cos(np.deg2rad(rotation.z)) - y * np.sin(np.deg2rad(rotation.z))
+        temp.y = x * np.sin(np.deg2rad(rotation.z)) + y * np.cos(np.deg2rad(rotation.z))
 
-        temp.x = origin.x + np.cos(calc_rot.y) * xz
-        temp.z = origin.z + np.sin(calc_rot.y) * xz
+        y = temp.y
+        z = temp.z
 
-        calc_rot.x = np.deg2rad(rotation.x) + np.atan2(temp.y - origin.y, temp.z - origin.z)
+        temp.y = y * np.cos(np.deg2rad(rotation.x)) - z * np.sin(np.deg2rad(rotation.x))
+        temp.z = y * np.sin(np.deg2rad(rotation.x)) + z * np.cos(np.deg2rad(rotation.x))
 
-        temp.z = origin.z + np.cos(calc_rot.x) * yz
-        temp.y = origin.y + np.sin(calc_rot.x) * yz
+        temp.x += origin.x
+        temp.y += origin.y
+        temp.z += origin.z
 
         return temp
     def add(self, rotation):
@@ -64,11 +68,12 @@ class Rotation():
 
 class camera():
     def __init__(self, position=Position(), rotation=Rotation()):
-        self.position = Position()
-        self.rotation = Rotation()
-    def move(self, x, y):
+        self.position = Position(position.x, position.y, position.z)
+        self.rotation = Rotation(rotation.x, rotation.y, rotation.z)
+    def move(self, x, y, z):
         self.position.x += x
         self.position.y += y
+        self.position.z += z
 
 class point():
     def __init__(self, position=Position()):
@@ -78,16 +83,18 @@ class point():
             self.position = Position()
         self.proj_x = 0
         self.proj_y = 0
-    def calc_pos(self, cam, fc=30):
-        temp_x = (self.position.x - cam.position.x)
-        temp_y = (self.position.y - cam.position.y)
-        temp_z = (self.position.z - cam.position.z)
-        if temp_z == 0:
+    def calc_pos(self, cam, fc=310):
+        temp = Position(self.position.x, self.position.y, self.position.z)
+        temp = Rotation.rotate(self.position, cam.position, cam.rotation)
+        temp.x -= cam.position.x
+        temp.y -= cam.position.y
+        temp.z -= cam.position.z
+        if temp.z == 0:
             self.proj_x = 0
             self.proj_y = 0
         else:
-            self.proj_x = (temp_x / temp_z) * fc
-            self.proj_y = (temp_y / temp_z) * fc
+            self.proj_x = (temp.x / temp.z) * fc
+            self.proj_y = (temp.y / temp.z) * fc
         return self.proj_x, self.proj_y
     def draw(self):
         global t
@@ -160,10 +167,11 @@ class Plane:
         for i in range(4):
             self.points.append(point())
     def draw(self, cam):
-        self.points[0].position = Position(self.position.x - self.width, self.position.y - self.height, self.position.z)
-        self.points[1].position = Position(self.position.x + self.width, self.position.y - self.height, self.position.z)
-        self.points[2].position = Position(self.position.x + self.width, self.position.y + self.height, self.position.z)
-        self.points[3].position = Position(self.position.x - self.width, self.position.y + self.height, self.position.z)
+        global screen
+        self.points[0].position = Position(self.position.x - self.width/2, self.position.y - self.height/2, self.position.z)
+        self.points[1].position = Position(self.position.x + self.width/2, self.position.y - self.height/2, self.position.z)
+        self.points[2].position = Position(self.position.x + self.width/2, self.position.y + self.height/2, self.position.z)
+        self.points[3].position = Position(self.position.x - self.width/2, self.position.y + self.height/2, self.position.z)
         self.points[0].position = Rotation.rotate(self.points[0].position, self.position, self.rotation)
         self.points[1].position = Rotation.rotate(self.points[1].position, self.position, self.rotation)
         self.points[2].position = Rotation.rotate(self.points[2].position, self.position, self.rotation)
@@ -171,9 +179,48 @@ class Plane:
         global t
         t.penup()
         for i in range(4):
+            i2 = (i+1) % len(self.points)
             self.points[i].calc_pos(cam)
+            self.points[i2].calc_pos(cam)
+            #clipping fixes
+            if np.abs(self.points[i].proj_x) >= screen.window_width()/2:
+                temp_slope = (self.points[i].proj_y - self.points[i2].proj_y)/(self.points[i].proj_x - self.points[i2].proj_x)
+                temp_inter = self.points[i].proj_y - (temp_slope * self.points[i].proj_x)
+                if self.points[i].proj_x > 0:
+                    self.points[i].proj_x = screen.window_width()/2
+                if self.points[i].proj_x < 0:
+                    self.points[i].proj_x = -screen.window_width()/2
+                self.points[i].proj_y = self.points[i].proj_x * temp_slope + temp_inter
+            if np.abs(self.points[i].proj_y) >= screen.window_height()/2:
+                temp_slope = (self.points[i].proj_x - self.points[i2].proj_x)/(self.points[i].proj_y - self.points[i2].proj_y)
+                temp_inter = self.points[i].proj_x - (temp_slope * self.points[i].proj_y)
+                if self.points[i].proj_y > 0:
+                    self.points[i].proj_y = screen.window_height()/2
+                if self.points[i].proj_y < 0:
+                    self.points[i].proj_y = -screen.window_height()/2
+                self.points[i].proj_x = self.points[i].proj_y * temp_slope + temp_inter
+            if np.abs(self.points[i2].proj_x) >= screen.window_width()/2:
+                temp_slope = (self.points[i].proj_y - self.points[i2].proj_y)/(self.points[i].proj_x - self.points[i2].proj_x)
+                temp_inter = self.points[i2].proj_y - (temp_slope * self.points[i2].proj_x)
+                if self.points[i2].proj_x > 0:
+                    self.points[i2].proj_x = screen.window_width()/2
+                if self.points[i2].proj_x < 0:
+                    self.points[i2].proj_x = -screen.window_width()/2
+                self.points[i2].proj_y = (self.points[i2].proj_x * temp_slope) + temp_inter
+            if np.abs(self.points[i2].proj_y) >= screen.window_height()/2:
+                temp_slope = (self.points[i].proj_x - self.points[i2].proj_x)/(self.points[i].proj_y - self.points[i2].proj_y)
+                temp_inter = self.points[i2].proj_x - (temp_slope * self.points[i2].proj_y)
+                if self.points[i2].proj_y > 0:
+                    self.points[i2].proj_y = screen.window_height()/2
+                if self.points[i2].proj_y < 0:
+                    self.points[i2].proj_y = -screen.window_height()/2
+                self.points[i2].proj_x = self.points[i2].proj_y * temp_slope + temp_inter
+            t.penup()
             t.goto(self.points[i].proj_x, self.points[i].proj_y)
             t.pendown()
+            t.goto(self.points[i2].proj_x, self.points[i2].proj_y)
+            t.penup()
+
         t.goto(self.points[0].proj_x, self.points[0].proj_y)
         t.penup()
 
@@ -185,33 +232,51 @@ class cube():
         self.height = height
         self.depth = depth
         self.planes = []
-        self.planes.append(Plane(self.position.add(0, 0, depth), self.rotation.add(Rotation(0,90,0)), self.width, self.height))
-        self.planes.append(Plane(self.position.add(0, 0, -depth), self.rotation.add(Rotation(0, 90, 0)), self.width, self.height))
-        self.planes.append(Plane(self.position.add(width, 0, 0), self.rotation.add(Rotation(0, 90, 0)), self.width, self.height))
+        self.planes.append(Plane(self.position.add(0, 0, depth*0.5), self.rotation.add(Rotation(0,0,0)), self.width, self.height))
+        self.planes.append(Plane(self.position.add(0, 0, depth*-0.5), self.rotation.add(Rotation(0, 0, 0)), self.width, self.height))
+        self.planes.append(Plane(self.position.add(width*-0.5, 0, 0), self.rotation.add(Rotation(0, 90, 0)), self.width, self.height))
+        self.planes.append(Plane(self.position.add(width*0.5, 0, 0), self.rotation.add(Rotation(0, 90, 0)), self.width,self.height))
+        self.planes.append(Plane(self.position.add(0, height*0.5, 0), self.rotation.add(Rotation(90, 0, 0)), self.width,self.height))
+        self.planes.append(Plane(self.position.add(0, height*-0.5, 0), self.rotation.add(Rotation(90, 0, 0)), self.width, self.height))
     def draw(self, cam):
         for i in range(len(self.planes)):
             self.planes[i].draw(cam)
 
+def get_mouse_pos(event):
+    global mouse_x, mouse_y
+    mouse_x = event.x - screen.window_width() / 2
+    mouse_y = screen.window_height() / 2 - event.y
+    print(mouse_x, mouse_y)
 
+def cam_mouse_move(mouse_x, mouse_y, cam, sensitivity = 40):
+    cam.rotation.y = mouse_x * sensitivity / 100
+    #add code here for rotation based on current orientation
 
 t = turtle.Pen()
 screen = Screen()
+screen.setup(width=1000, height=1000)
+canvas = screen.getcanvas()
+canvas.bind('<Motion>', get_mouse_pos)
 turtle.Screen().bgcolor("white")
+turtle.tracer(0)
 
-cam = camera(Position(0, 0, 30))
+mouse_x, mouse_y = 0,0
 
-cube1 = cube(position=Position(0,0,10), rotation=Rotation(0,0,0), width=30, height=30, depth=30)
+cam = camera(Position(0, 0, 30), Rotation(0,0,0))
+
+cube1 = cube(position=Position(0,0,0), rotation=Rotation(0,0,0), width=15, height=20, depth=32)
+#plane1 = Plane(Position(0,0,0),Rotation(0,90,0),8,8)
 
 t.speed(0)
-
 
 turtle.reset()
 while True:
     screen.listen()
-    screen.onkeypress(lambda: cam.move(1,0), "Right")
-    screen.onkeypress(lambda: cam.move(-1, 0), "Left")
-    screen.onkeypress(lambda: cam.move(0, 1), "Up")
-    screen.onkeypress(lambda: cam.move(0, -1), "Down")
+    screen.onkeypress(lambda: cam.move(-1,0, 0), "Right")
+    screen.onkeypress(lambda: cam.move(1, 0, 0), "Left")
+    screen.onkeypress(lambda: cam.move(0, -1, 0), "Up")
+    screen.onkeypress(lambda: cam.move(0, 1, 0), "Down")
+    cam_mouse_move(mouse_x, mouse_y, cam)
     t.reset()
-    screen.update()
     cube1.draw(cam)
+    turtle.update()
